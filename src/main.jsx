bb14bf1c7697c81386d21f1f2548910df155325b
Advait Tonpe <advaittonpe11@gmail.com>
2026-05-28 22:14:27 +0530

    Added CartContext correctly

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CartProvider from "../src/context/CartContext.jsx";
+import CartProvider from "./context/CartContext.jsx";
+
 createRoot(document.getElementById('root')).render(
-  <CartProvider>
-    <App />
+  <StrictMode>
+    <CartProvider>
+      <App />
     </CartProvider>
-  
+  </StrictMode>
 )
